2717ef6f6856d10669d075f3153710fe8b0012e6
Thomas Bellembois <thomas.bellembois@uca.fr>
2023-08-30 14:40:41 +0200

    Implement display for CommonError.

--- src/types/error.rs
+++ src/types/error.rs
@@ -1,3 +1,8 @@
+use std::fmt;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Clone)]
 pub enum CommonError {
     NotFound,
     InvalidQueryParamater(String),
@@ -5,4 +10,25 @@ pub enum CommonError {
     InvalidDirectoryName,
     InternalServerError(String),
     Timeout(String),
+}
+
+// Implement std::fmt::Display for CommonError
+impl fmt::Display for CommonError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CommonError::NotFound => write!(f, "not found"),
+            CommonError::InvalidQueryParamater(p) => write!(f, "invalid query parameter {}", p),
+            CommonError::MissingQueryParamater(p) => write!(f, "missing query parameter {}", p),
+            CommonError::InvalidDirectoryName => write!(f, "invalid directory name"),
+            CommonError::InternalServerError(e) => write!(f, "internal server error {}", e),
+            CommonError::Timeout(t) => write!(f, "timeout {}", t),
+        }
+    }
+}
+
+// Implement std::fmt::Debug for CommonError
+impl fmt::Debug for CommonError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{ file: {}, line: {} }}", file!(), line!()) // programmer-facing output
+    }
 }
