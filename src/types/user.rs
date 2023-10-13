1fe4d0e2a3556fb41d188e75fcb000235d0474ee
Thomas Bellembois <thomas.bellembois@uca.fr>
2023-10-13 10:59:19 +0200

    Implement Ord for User.

--- src/types/user.rs
+++ src/types/user.rs
@@ -1,6 +1,6 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, PartialOrd, Eq, Ord)]
 pub struct User {
     pub id: String,
     pub display: String,
