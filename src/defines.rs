385db5bd2dbc73a5cc8df569c7053f7bb5f7b73c
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-01-09 13:51:18 +0100

    Add ROOT_DIRECTORY_NAME_RE_STRING.

--- src/defines.rs
+++ src/defines.rs
@@ -1,3 +1,4 @@
-pub const DIRECTORY_NAME_RE_STRING: &str = r"^[a-zA-Z0-9@_-]+$";
+pub const ROOT_DIRECTORY_NAME_RE_STRING: &str = r"^[a-zA-Z0-9@_-]+$";
+pub const DIRECTORY_NAME_RE_STRING: &str = r"^[a-zA-Z0-9_-]+$";
 pub const USER_CN_RE_STRING: &str = r"^uid=([a-zA-Z0-9_\.]+),.+$";
 pub const GROUP_CN_RE_STRING: &str = r"^[a-zA-Z0-9_]+$";
