4b71add81d814e786ff469b1a556d1864abff3be
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-01-15 16:18:48 +0100

    Derive Debug for User.

--- src/types/user.rs
+++ src/types/user.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, PartialOrd, Eq, Ord, Debug)]
 pub struct User {
     pub id: String,
     pub display: String,
