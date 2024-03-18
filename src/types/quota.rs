6e054430386e2fae52dedb6171b9c5754eccd3f3
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-03-18 17:16:21 +0100

    Add QuotaUnit.

--- src/types/quota.rs
+++ src/types/quota.rs
@@ -1,7 +1,26 @@
+use std::fmt;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct SetQuota {
     pub name: String,
-    pub quota: u32,
+    pub quota: u64,
+}
+
+#[derive(PartialEq)]
+pub enum QuotaUnit {
+    Megabyte,
+    Gigabyte,
+    Terabyte,
+}
+
+impl fmt::Display for QuotaUnit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            QuotaUnit::Megabyte => write!(f, "MiB"),
+            QuotaUnit::Gigabyte => write!(f, "GiB"),
+            QuotaUnit::Terabyte => write!(f, "TiB"),
+        }
+    }
 }
